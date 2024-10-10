c76aac101ad0858898acd26b2ad40f5f8f205a5a
Student Noname <Student@lan.ecol.edu.ru>
2024-10-10 11:19:28 +0500

    header

--- auth/logout.php
+++ auth/logout.php
@@ -2,3 +2,5 @@
 
 session_start();
 $_SESSION['user'] = null;
+
+header('Location: /');
--- header.php
+++ header.php
@@ -1,23 +1,23 @@
 <?php 
 
     session_start();
-//    echo $_SESSION["user"]["role"] ?? "не вошел";
-//    echo
+    $isLoggedIn = isset($_SESSION["user"]);
 ?>
 
 <header class="flex m-10 gap-5 justify-between">
     <div class="flex gap-5">
         <a href="/">комментарии</a>
         <a href="/jokes.php">анекдоты</a>
+        <?php if ($isLoggedIn) echo "<a href='/bulletin_board/board.php'>доска объявлений</a>" ?>
     </div>
     <form class="flex gap-3" action="">
     <?php
-    echo isset($_SESSION["user"]) && $_SESSION["user"]["role"] == 'ADMIN'
+    echo $isLoggedIn && $_SESSION["user"]["role"] == 'ADMIN'
         ? '<a href="/admin.php" class="bg-sky-700 p-3 text-white">ADMIN PANEL</a>'
         : '';
-    echo isset($_SESSION["user"])
+    echo $isLoggedIn
         ? '<a href="/auth/logout.php" class="bg-sky-700 p-3 text-white">LOGOUT</a>'
-        : '';
+        : '<a href="/auth/login.php" class="bg-sky-700 p-3 text-white">LOGIN</a>';
     ?>
     </form>
 </header>
